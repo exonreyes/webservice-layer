c24bc3b203483ac421a81ad091c2df3cbb71c1b0
Pablo Reyes <p.reyes1704@gmail.com>
2024-11-06 08:28:03 -0600

    Se actualiza estado del ticket

--- src/main/java/nova/business/service/ActividadServiceImpl.java
+++ src/main/java/nova/business/service/ActividadServiceImpl.java
@@ -5,15 +5,20 @@ import lombok.RequiredArgsConstructor;
 import nova.business.service.factory.TicketFactory;
 import nova.common.EntityException;
 import nova.domain.entity.ActividadTicket;
+import nova.domain.entity.Estatus;
+import nova.domain.entity.Ticket;
 import nova.persistence.ActividadTicketRepository;
+import nova.persistence.TicketRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class ActividadServiceImpl implements IActividadService {
     private final ActividadTicketRepository repository;
+    private final TicketRepository ticketRepository;
 
     @Override
     public List<ActividadTicket> getActividades(Integer ticketId) {
@@ -26,6 +31,14 @@ public class ActividadServiceImpl implements IActividadService {
         var data = TicketFactory.createActividadTicket(newActividad);
         try {
             repository.save(data);
+            Optional<Ticket> ticketModel = ticketRepository.findById(data.getTicket().getId());
+            if (ticketModel.isPresent()) {
+                var tempTicket=ticketModel.get();
+                tempTicket.setEstatus(new Estatus(newActividad.getEstatus().getId()));
+                ticketRepository.save(tempTicket);
+            }else {
+                throw new EntityException("E1003","No se pudo guardar, ticket no disponible");
+            }
         } catch (Exception e) {
             throw new EntityException("E1003", "Error al registra la actividad");
         }
